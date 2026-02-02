74efba3cb1d2eabebb05271198d1695187af07b4
netizen-0 <rishabhytpremium17@gmail.com>
2026-02-02 10:03:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DELETE_DELAY = int(os.getenv("DELETE_DELAY", 900))  # default 15 minutes
+DELETE_DELAY = int(os.getenv("DELETE_DELAY", 300))  # default 5 minutes
